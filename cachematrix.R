33f040708b345ab72d8bb839f73741edb3a1aa74
basuniladri <basuniladri@rediffmail.com>
2016-09-26 17:54:09 +0530

    Updated cahceMatrix Code

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse 
+    getInverse <- function() inv
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
+## Need to install matrixcalc package
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+   
+   if( !is.singular.matrix(x$get()) ){
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+      message("getting Cached Matrix Inverse Data.")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv
+  }
+ else {
+   message("Singular Matrix doesn't have inverse.")
+   inv <- NULL
+  }
 }
+
+
